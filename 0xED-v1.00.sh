3792dc04d12a972407aa4c66e5a5a5d52aabbb91
redinmatvey <redinmatvey11@gmail.com>
2025-07-06 20:00:35 +0300

    Update 0xED-v1.00.sh

--- 0xED-v1.00.sh
+++ 0xED-v1.00.sh
@@ -12,8 +12,8 @@ print_help() {
     echo
 }
 
-echo -e "${RED}=== 0xED - простой текстовый редактор ===${NC}"
-echo -e "${RED}Введите текст. Для справки введите ':h' и нажмите Enter.${NC}"
+echo -e "${RED}=== 0xED - простой текстовый редактор ==>
+echo -e "${RED}Введите текст. Для справки введите ':h' >
 
 buffer=()
 while true; do
@@ -21,18 +21,10 @@ while true; do
     echo -ne "${RED}> ${NC}"
     read line
     if [[ "$line" == ":w" ]]; then
-        echo -ne "${RED}Введите имя файла для сохранения: ${NC}"
+        echo -ne "${RED}Введите имя файла для сохранени>
         read filename
         printf "%s\n" "${buffer[@]}" > "$filename"
-        echo -e "${RED}Файл сохранён в '$filename'.${NC}"
+        echo -e "${RED}Файл сохранён в '$filename'.${NC>
         break
     elif [[ "$line" == ":q" ]]; then
         echo -e "${RED}Выход без сохранения.${NC}"
-        break
-    elif [[ "$line" == ":h" ]]; then
-        print_help
-    else
-        buffer+=("$line")
-    fi
-done
-
