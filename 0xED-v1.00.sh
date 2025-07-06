72f7bfb60a140f35bca4f4b4ef7558189bb760e6
redinmatvey <redinmatvey11@gmail.com>
2025-07-06 20:04:11 +0300

    Update 0xED-v1.00.sh

--- 0xED-v1.00.sh
+++ 0xED-v1.00.sh
@@ -4,21 +4,14 @@
 RED='\033[0;31m'
 NC='\033[0m' # без цвета
 
-print_help() {
-    echo -e "${RED}Команды редактора 0xED:${NC}"
-    echo -e "${RED}:w${NC} - сохранить текст в файл"
-    echo -e "${RED}:q${NC} - выйти без сохранения"
-    echo -e "${RED}:h${NC} - показать справку"
-    echo
-}
-
 echo -e "${RED}=== 0xED - простой текстовый редактор ==>
-echo -e "${RED}Введите текст. Для справки введите ':h' >
+echo -e "${RED}Введите текст. Для сохранения введите ':>
+echo -e "${RED}Для выхода без сохранения введите ':q' и>
 
 buffer=()
 while true; do
-    # Выводим приглашение ">" красным цветом
-    echo -ne "${RED}> ${NC}"
+    # Выводим приглашение "1>" красным цветом
+    echo -ne "${RED}1> ${NC}"
     read line
     if [[ "$line" == ":w" ]]; then
         echo -ne "${RED}Введите имя файла для сохранени>
@@ -28,3 +21,8 @@ while true; do
         break
     elif [[ "$line" == ":q" ]]; then
         echo -e "${RED}Выход без сохранения.${NC}"
+        break
+    else
+        buffer+=("$line")
+    fi
+done
